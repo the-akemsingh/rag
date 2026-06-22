3eb4524745109ef9acb3fe5a4655640a31336982
the-akemsingh <singhakem03@gmail.com>
2026-06-22 11:00:44 +0530

    fix : typo

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://askdocs-xi.vercel.app/"
+        "https://askdocs-xi.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
